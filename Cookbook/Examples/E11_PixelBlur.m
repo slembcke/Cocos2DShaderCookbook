b48ba60247d992cf69f04f19d550d20591636351
Andy Korth <andy@howlingmoonsoftware.com>
2014-05-07 14:24:06 -0500

    Comments for the pixel blur example

--- Cookbook/Examples/E11_PixelBlur.m
+++ Cookbook/Examples/E11_PixelBlur.m
@@ -12,16 +12,16 @@
 	CCSprite *sprite = [CCSprite spriteWithImageNamed:@"Logo.png"];
 	sprite.shader = [CCShader shaderNamed:self.shaderName];
 
+	// Load the distortion texture, a noise texture which we use to determine how to offset individual fragments when we draw them.
 	CCTexture* distortion = [[CCTextureCache sharedTextureCache] addImage:@"gaussianBlur.png"];
 	// Nearest neighboor interpolating to create a pixely effect out of the distortion texture.
 	ccTexParams params = {GL_NEAREST, GL_NEAREST, GL_REPEAT, GL_REPEAT};
 	[distortion setTexParameters:&params];
 	
 	sprite.shaderUniforms[@"u_DistortionTexture"] = distortion;
+	// We use the texture's size, so we can scale the distortion to match the aspect ratio of the image.
 	sprite.shaderUniforms[@"u_mainTextureSize"] = [NSValue valueWithCGSize:distortion.contentSizeInPixels];
 
-	
-	
 	ColorSlider *distortionSlider = [ColorSlider node];
 	distortionSlider.preferredSize = CGSizeMake(sprite.contentSize.width, 32);
 	distortionSlider.endColor = [CCColor colorWithRed:0 green:0 blue:0 alpha:0];
@@ -30,7 +30,7 @@
 	ColorSlider *animationSlider = [ColorSlider node];
 	animationSlider.preferredSize = CGSizeMake(sprite.contentSize.width, 32);
 	animationSlider.endColor = [CCColor colorWithRed:0 green:0 blue:0 alpha:0];
-	// Round the number so it's on or off
+	// Round the number so it's on or off. 0 will disable the animation, 1 will enable it.
 	animationSlider.colorBlock = ^(CCColor *color){sprite.shaderUniforms[@"u_AnimationEnabled"] = [NSNumber numberWithFloat: roundf(color.red)];};
 	
 	ColorSlider *blockSizeSlider = [ColorSlider node];
--- Cookbook/Examples/E11_PixelBlur.vsh
+++ Cookbook/Examples/E11_PixelBlur.vsh
@@ -10,9 +10,14 @@ void main(){
 	cc_FragColor = clamp(cc_Color, 0.0, 1.0);
 	cc_FragTexCoord1 = cc_TexCoord1;
 	cc_FragTexCoord2 = cc_TexCoord2;
-	
+
+	// We do as much work as possible in the vertex shader, and pass the results to the fragment shader.
+	// Here we calculate a properly scaled UV coordinates...
 	vec2 scaleDistortion = u_mainTextureSize / max(u_mainTextureSize.x, u_mainTextureSize.y);
+	// Apply a random offset, if animation is enabled.
 	vec2 randomAnimatedOffset = cc_Random01.xy * u_AnimationEnabled;
+	// pass our UV coordinates to the fragment shader for the texture lookups. BlockSize allows us to "zoom in" on the
+	// distortion texture, so the distortion blocks are like giant pixels.
 	v_distortionCoords = (cc_FragTexCoord1 + randomAnimatedOffset) * u_BlockSize * scaleDistortion;
 	
 	
